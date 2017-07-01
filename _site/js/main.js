d308be9b207982f515b1c6161108e5b7bb5c60c4
P Seetaramaraju <psramaraju@gmail.com>
2017-07-01 21:10:20 +0530

    loader based changes
    loader based changes.

--- _site/js/main.js
+++ _site/js/main.js
@@ -4,6 +4,31 @@ $(document).ready(function(){
       $(this).parent().toggleClass('active');
     }); 
 
+var COLORS = [['006E00', '78E600'], ['68008C', '78E600'],
+              ['006E00', '00BEFF'], ['033CD2', '00BEFF'],
+              ['6E008C', '00BEFF'], ['A00000', 'FF7800'],
+              ['033CD2', 'FF64D2'], ['6E008C', 'FF64D2'],
+              ['6E008C', 'FF7800'], ['033CD2', 'FF7800'],
+              ['006E00', 'FFD732'], ['6E008C', 'FFD732']];
+
+var color = Math.floor((Math.random() * 12));
+
+var logoID = color + 1;
+
+if (color < 10) {
+    logoID = "0" + logoID;
+}
+
+document.querySelector('#defaultIcon1').href = 'https://www.sugarlabs.org/assets/favicon_' + logoID + '.png';
+var h = document.querySelector('.logo1').innerHTML;
+h = h.replace(/033cd2/g, COLORS[color][0]);
+h = h.replace(/78e600/g, COLORS[color][1]);
+document.querySelector('.logo1').innerHTML = h;
+// document.querySelector('#defaultIcon1').href = 'https://www.sugarlabs.org/assets/favicon_' + logoID + '.png';
+var h = document.querySelector('.logo2').innerHTML;
+h = h.replace(/033cd2/g, COLORS[color][0]);
+h = h.replace(/78e600/g, COLORS[color][1]);
+document.querySelector('.logo2').innerHTML = h;
 document.getElementById("mcw1").style.display = "none";
 
 document.getElementById("mcw2").style.display = "none";
@@ -85,26 +110,26 @@ var fullHeight = function() {
 	});
 };
 	
-var COLORS = [['006E00', '78E600'], ['68008C', '78E600'],
-              ['006E00', '00BEFF'], ['033CD2', '00BEFF'],
-              ['6E008C', '00BEFF'], ['A00000', 'FF7800'],
-              ['033CD2', 'FF64D2'], ['6E008C', 'FF64D2'],
-              ['6E008C', 'FF7800'], ['033CD2', 'FF7800'],
-              ['006E00', 'FFD732'], ['6E008C', 'FFD732']];
+// var COLORS = [['006E00', '78E600'], ['68008C', '78E600'],
+//               ['006E00', '00BEFF'], ['033CD2', '00BEFF'],
+//               ['6E008C', '00BEFF'], ['A00000', 'FF7800'],
+//               ['033CD2', 'FF64D2'], ['6E008C', 'FF64D2'],
+//               ['6E008C', 'FF7800'], ['033CD2', 'FF7800'],
+//               ['006E00', 'FFD732'], ['6E008C', 'FFD732']];
 
-var color = Math.floor((Math.random() * 12));
+// var color = Math.floor((Math.random() * 12));
 
-var logoID = color + 1;
+// var logoID = color + 1;
 
-if (color < 10) {
-    logoID = "0" + logoID;
-}
+// if (color < 10) {
+//     logoID = "0" + logoID;
+// }
 
-document.querySelector('#defaultIcon1').href = 'https://www.sugarlabs.org/assets/favicon_' + logoID + '.png';
-var h = document.querySelector('.logo1').innerHTML;
-h = h.replace(/033cd2/g, COLORS[color][0]);
-h = h.replace(/78e600/g, COLORS[color][1]);
-document.querySelector('.logo1').innerHTML = h;
+// document.querySelector('#defaultIcon1').href = 'https://www.sugarlabs.org/assets/favicon_' + logoID + '.png';
+// var h = document.querySelector('.logo2').innerHTML;
+// h = h.replace(/033cd2/g, COLORS[color][0]);
+// h = h.replace(/78e600/g, COLORS[color][1]);
+// document.querySelector('.logo2').innerHTML = h;
 });
 
 

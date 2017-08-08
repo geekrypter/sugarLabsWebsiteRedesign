733e5094c7f3b3c6a4c6ce39a3e023c48095f7ea
P Seetaramaraju <psramaraju@gmail.com>
2017-08-08 11:00:44 +0530

    New design changes
    New design changes.

--- _site/js/main.js
+++ _site/js/main.js
@@ -181,7 +181,7 @@ var isChrome = !!window.chrome && !!window.chrome.webstore;
 
 
 if(isOpera || isFirefox || isSafari || isIE || isEdge || isChrome){
-document.getElementById("loaderDiv").style.display="block";
+// document.getElementById("loaderDiv").style.display="block";
 document.getElementById("myiframe1").src = 'https://kiwiirc.com/client/irc.kiwiirc.com/?&theme=cli#sugar';
 document.getElementById("myiframe2").src = 'https://kiwiirc.com/client/irc.kiwiirc.com/?&theme=cli#sugar';
 
